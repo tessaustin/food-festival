493639ef1b61feb2e9ae47a647de8512d259a640
Tess Austin <tessaustin92@gmail.com>
2021-05-25 11:31:54 -0600

    Code split the app to separate concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack")
 const path = require("path");
 
@@ -8,10 +9,13 @@ module.exports = {
     filename: "main.bundle.js"
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
   ],
   mode: "development"
 };
